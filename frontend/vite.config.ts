bbaf9dbce66a514e4e5d93dca79ab92ac7f1e29c
jackdzi <jackdzialoa@gmail.com>
2025-03-10 03:47:14 -0500

    whitelist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
+  server: {
     allowedHosts: ['csvanalysis.up.railway.app'],
   },
 })
